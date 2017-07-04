89491f9846ade9735837127f3d24b70a7196700f
Ayushman Dash <dash.ayushman.99@gmail.com>
2017-07-04 14:45:15 +0200

    initialized project

---
+++ model.py
@@ -0,0 +1,202 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from Utils import ops
+
+
+class AutoGAN :
+	'''
+	OPTIONS
+	z_dim : Noise dimension 100
+	t_dim : Text feature dimension 256
+	image_size : Image Dimension 64
+	gf_dim : Number of conv in the first layer generator 64
+	df_dim : Number of conv in the first layer discriminator 64
+	gfc_dim : Dimension of gen untis for for fully connected layer 1024
+	caption_vector_length : Caption Vector Length 2400
+	batch_size : Batch Size 64
+	'''
+
+	def __init__(self, options) :
+		self.options = options
+
+	def build_model(self) :
+
+		print('Initializing placeholder')
+		img_size = self.options['image_size']
+		image = tf.placeholder('float32', [self.options['batch_size'],
+		                              img_size, img_size, 3],
+		                              name = 'input_image')
+
+		classes = tf.placeholder('float32', [self.options['batch_size'],
+										self.options['n_classes']],
+		                                name='real_classes')
+
+		training = tf.placeholder(tf.bool, name='training')
+
+		print('Building the Encoder')
+		en_image, en_image_shape = self.encoder(image,
+							self.options['n_classes'], training)
+
+		print('Building the Generator')
+		gen_image = self.decoder(en_image, training)
+
+		flat_image = tf.contrib.layers.flatten(image)
+		flat_gen_image = tf.contrib.layers.flatten(gen_image)
+
+		print('Building the Loss Function')
+		auto_gan_loss = tf.contrib.losses.mean_squared_error(flat_gen_image,
+															labels=flat_image)
+
+		self.add_tb_histogram_summaries()
+		self.add_tb_scalar_summaries(auto_gan_loss)
+
+		self.add_image_summary('Encoded Images', image,
+		                       self.options['batch_size'])
+		self.add_image_summary('Decoded Images', gen_image,
+							   self.options['batch_size'])
+
+		t_vars = tf.trainable_variables()
+		ag_vars = [var for var in t_vars if 'e_' in var.name or 'd_' in
+				   var.name]
+
+		input_tensors = {
+			'input_images' : image,
+			'training' : training,
+
+		}
+
+		variables = {
+			'ag_vars' : ag_vars
+		}
+
+		loss = {
+			'autogan_loss' : auto_gan_loss,
+		}
+
+		outputs = {
+			'decoder' : gen_image
+		}
+
+		checks = {
+			'auto_gan_loss': auto_gan_loss,
+			'decoder': gen_image
+		}
+
+		return input_tensors, variables, loss, outputs, checks
+
+	def add_tb_histogram_summaries(self, t_vars):
+
+		print('List of all variables')
+		for v in t_vars:
+			print(v.name)
+			print(v)
+			self.add_histogram_summary(v.name, v)
+
+	def add_tb_scalar_summaries(self, autogan_loss):
+		self.add_scalar_summary("AutoGAN_Loss", autogan_loss)
+
+	def add_scalar_summary(self, name, var):
+		tf.summary.scalar(name, var)
+
+	def add_histogram_summary(self, name, var):
+		with tf.name_scope('summaries'):
+			tf.summary.histogram(name, var)
+
+	def add_image_summary(self, name, var, max_outputs=1):
+		with tf.name_scope('summaries'):
+			tf.summary.image(name, var, max_outputs=max_outputs)
+
+	# GENERATOR IMPLEMENTATION based on :
+	# https://github.com/carpedm20/DCGAN-tensorflow/blob/master/model.py
+	def decoder(self, en_image, t_training):
+
+		s = self.options['image_size']
+		s2, s4, s8, s16, s32 = int(s / 2), int(s / 4), int(s / 8),\
+							   int(s / 16), int(s / 32)
+
+		h1 = ops.deconv2d(en_image, [self.options['batch_size'], s16, s16,
+		                       self.options['df_dim'] * 4], name = 'd_h1')
+		h1 = tf.nn.relu(slim.batch_norm(h1, is_training = t_training,
+		                                scope="d_bn1"))
+
+		h2 = ops.deconv2d(h1, [self.options['batch_size'], s8, s8,
+		                       self.options['df_dim'] * 6], name = 'd_h2')
+		h2 = tf.nn.relu(slim.batch_norm(h2, is_training = t_training,
+		                                scope="d_bn2"))
+		
+		h3 = ops.deconv2d(h2, [self.options['batch_size'], s4, s4,
+		                       self.options['df_dim'] * 6], name = 'd_h3')
+		h3 = tf.nn.relu(slim.batch_norm(h3, is_training = t_training,
+		                                scope="d_bn3"))
+
+		h4 = ops.deconv2d(h3, [self.options['batch_size'], s2, s2,
+		                       self.options['df_dim'] * 8], name = 'd_h4')
+		h4 = tf.nn.relu(slim.batch_norm(h4, is_training = t_training,
+		                                scope="d_bn4"))
+
+		h5 = ops.deconv2d(h4, [self.options['batch_size'], s, s, 3],
+		                  name = 'd_h5')
+
+		return (tf.tanh(h5) / 2. + 0.5)
+
+
+	# DISCRIMINATOR IMPLEMENTATION based on :
+	# https://github.com/carpedm20/DCGAN-tensorflow/blob/master/model.py
+	def encoder(self, image, n_classes, t_training, reuse = False) :
+
+		if reuse :
+			tf.get_variable_scope().reuse_variables()
+
+		h0 = ops.lrelu(ops.conv2d(image, self.options['ef_dim'],
+								  name = 'd_h0_conv'))  # 64
+
+		h1 = ops.lrelu(slim.batch_norm(ops.conv2d(h0,
+		                                     self.options['ef_dim'] * 8,
+		                                     name = 'e_h1_conv'),
+		                               reuse=reuse,
+		                               is_training = t_training,
+		                               scope = 'e_bn1'))  # 32
+
+		h2 = ops.lrelu(slim.batch_norm(ops.conv2d(h1,
+		                                     self.options['ef_dim'] * 6,
+		                                     name = 'e_h2_conv'),
+		                               reuse=reuse,
+		                               is_training = t_training,
+		                               scope = 'e_bn2'))  # 16
+		h3 = ops.lrelu(slim.batch_norm(ops.conv2d(h2,
+		                                     self.options['ef_dim'] * 4,
+		                                     name = 'e_h3_conv'),
+		                               reuse=reuse,
+		                               is_training = t_training,
+		                               scope = 'e_bn3'))  # 8
+
+		h4 = ops.lrelu(slim.batch_norm(ops.conv2d(h3,
+												self.options['ef_dim'] * 2,
+													  1, 1, 1, 1,
+												name = 'e_h3_conv_new'),
+		                                reuse=reuse,
+		                                is_training = t_training,
+		                                scope = 'e_bn4'))  # 4
+
+		h4_shape = h4.get_shape().as_list()
+		#h4_flat = tf.contrib.layers.flatten(h4)
+
+		#h5 = ops.linear(h4_flat, 1024, 'fl_e_01')
+		#h6 = ops.linear(h5, n_classes, 'fl_e_02')
+		
+		return h4, h4_shape
+
+	# This has not been used used yet but can be used
+	def attention(self, decoder_output, seq_outputs, output_size, time_steps,
+			reuse=False) :
+		if reuse:
+			tf.get_variable_scope().reuse_variables()
+		ui = ops.attention(decoder_output, seq_outputs, output_size,
+		                   time_steps, name = "g_a_attention")
+
+		with tf.variable_scope('g_a_attention'):
+			ui = tf.transpose(ui, [1, 0, 2])
+			ai = tf.nn.softmax(ui,  dim=1)
+			seq_outputs = tf.transpose(seq_outputs, [1, 0, 2])
+			d_dash = tf.reduce_sum(tf.mul(seq_outputs, ai), axis=1)
+			return d_dash, ai
---
+++ train.py
@@ -0,0 +1,251 @@
+import model
+import argparse
+import scipy.misc
+import os
+import shutil
+import skimage
+import progressbar
+
+import tensorflow as tf
+import numpy as np
+
+from os.path import join
+from tensorflow.examples.tutorials.mnist import input_data
+
+def main():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('--batch_size', type=int, default=64,
+						help='Batch Size')
+
+	parser.add_argument('--image_size', type=int, default=128,
+						help='Image Size a, a x a')
+
+	parser.add_argument('--df_dim', type=int, default=64,
+						help='Number of conv in the first layer gen.')
+
+	parser.add_argument('--ef_dim', type=int, default=64,
+						help='Number of conv in the first layer discr.')
+
+	parser.add_argument('--n_classes', type = int, default = 102,
+	                    help = 'Number of classes/class labels')
+
+	parser.add_argument('--data_dir', type=str, default="Data",
+						help='Data Directory')
+
+	parser.add_argument('--learning_rate', type=float, default=0.0002,
+						help='Learning Rate')
+
+	parser.add_argument('--beta1', type=float, default=0.5,
+						help='Momentum for Adam Update')
+
+	parser.add_argument('--epochs', type=int, default=200,
+						help='Max number of epochs')
+
+	parser.add_argument('--save_every', type=int, default=30,
+						help='Save Model/Samples every x iterations over '
+							 'batches')
+
+	parser.add_argument('--resume_model', type=bool, default=False,
+						help='Pre-Trained Model load or not')
+
+	parser.add_argument('--data_set', type=str, default="mnist",
+						help='Dat set: mnist, flowers')
+
+	parser.add_argument('--model_name', type=str, default="AutoGANDoc",
+						help='model_1 or model_2')
+
+	parser.add_argument('--train', type = bool, default = True,
+	                    help = 'True while training and otherwise')
+
+	parser.add_argument('--validate_every', type=int, default=1,
+						help='run validation after how many epochs')
+
+	args = parser.parse_args()
+
+	model_dir, model_chkpnts_dir, model_samples_dir, model_val_samples_dir,\
+							model_summaries_dir = initialize_directories(args)
+
+	data = load_training_data(data_dir=args.data_dir, data_set=args.data_set)
+	model_options = {
+		'batch_size': args.batch_size,
+		'image_size': args.image_size,
+		'df_dim': args.gf_dim,
+		'ef_dim': args.df_dim,
+		'n_classes': args.n_classes
+	}
+
+	# Initialize and build the GAN model
+	auto_gan = model.AutoGan(model_options)
+	input_tensors, variables, loss, outputs, checks = auto_gan.build_model()
+
+	ag_optim = tf.train.AdamOptimizer(args.learning_rate,
+						 beta1=args.beta1).minimize(loss['autogan_loss'],
+						 var_list=variables['ag_vars'])
+
+	global_step_tensor = tf.Variable(1, trainable=False, name='global_step')
+	merged = tf.summary.merge_all()
+	sess = tf.InteractiveSession()
+
+	summary_writer = tf.summary.FileWriter(model_summaries_dir, sess.graph)
+
+	tf.global_variables_initializer().run()
+	saver = tf.train.Saver(max_to_keep=10000)
+
+	if args.resume_model:
+		print('Trying to resume training from a previous checkpoint' +
+		      str(tf.train.latest_checkpoint(model_chkpnts_dir)))
+		if tf.train.latest_checkpoint(model_chkpnts_dir) is not None:
+			saver.restore(sess, tf.train.latest_checkpoint(model_chkpnts_dir))
+			print('Successfully loaded model. Resuming training.')
+		else:
+			print('Could not load checkpoints.  Training a new model')
+
+	global_step = global_step_tensor.eval()
+	gs_assign_op = global_step_tensor.assign(global_step)
+	history = {'training_losses': [], 'validation_losses': [],
+			   'best_loss':float("inf"), 'best_epoch': 0,
+			   'validate_every': args.validate_every}
+
+	for n_e in range(args.epochs):
+		print('Training Epoch {}\n\n'.format(n_e))
+		bar = progressbar.ProgressBar(redirect_stdout=True,
+				  max_value=int(data.train.num_examples()/args.batch_size))
+		batch_count = 0
+		training_batch_losses = []
+		while n_e == data.train.epochs_completed():
+			batch = data.train.next_batch(args.batch_size)
+			if args.data_set == 'mnist':
+				batch = process_mnist_images(batch)
+
+			# Encoder Update
+			_, ag_loss, decoded_images, summary = sess.run([ag_optim,
+                        loss['auto_gan_loss'], outputs['decoder'], merged],
+                        feed_dict={
+							input_tensors['input_images'].name : batch[0],
+							input_tensors['t_training'].name : args.train
+						})
+			training_batch_losses.append(ag_loss)
+			print("\nAG Loss: {}\n".format(ag_loss))
+			summary_writer.add_summary(summary, global_step)
+
+			global_step += 1
+			sess.run(gs_assign_op)
+			batch_count += 1
+			bar.update(batch_count)
+			if (batch_count % args.save_every) == 0 and batch_count != 0:
+				print("Saving Images and the Model\n\n")
+				save_for_vis(model_samples_dir, batch[0], decoded_images)
+				save_path = saver.save(sess, join(model_chkpnts_dir,
+						"latest_model_{}_temp.ckpt".format(args.data_set)))
+		mean_training_loss = np.mean(training_batch_losses)
+		history['training_losses'].append(mean_training_loss)
+		bar.finish()
+
+		# Save model after epoch
+		epoch_dir = join(model_chkpnts_dir, str(n_e))
+		if not os.path.exists(epoch_dir):
+			os.makedirs(epoch_dir)
+		save_path = saver.save(sess, join(epoch_dir,
+				"model_after_{}_epoch_{}.ckpt".format(args.data_set, n_e)))
+
+		if n_e % args.validate_every == 0:
+			print('\nValidating Samples\n')
+			bar = progressbar.ProgressBar(redirect_stdout=True,
+			  max_value=int(data.validation.num_examples() /args.batch_size))
+			batch_count = 0
+			val_batch_losses = []
+			while data.validation.epochs_completed() == 0:
+				batch = data.validation.next_batch(args.batch_size)
+				if args.data_set == 'mnist':
+					batch = process_mnist_images(batch)
+
+				shutil.rmtree(model_val_samples_dir)
+				os.makedirs(model_val_samples_dir)
+
+				ag_loss, decoded_images = sess.run(
+					[loss['auto_gan_loss'], outputs['decoder']],
+					    feed_dict={
+						   input_tensors['input_images'].name: batch[0],
+						   input_tensors['t_training'].name: args.train
+					    })
+				val_batch_losses.append(ag_loss)
+				batch_count += 1
+				bar.update(batch_count)
+				if (batch_count % args.save_every) == 0 and batch_count != 0:
+					save_for_vis(model_val_samples_dir, batch[0],
+								 decoded_images)
+			mean_val_loss = np.mean(val_batch_losses)
+			history['validation_losses'].append(mean_val_loss)
+			if mean_val_loss <= history['best_loss']:
+				history['best_loss'] = mean_val_loss
+				history['best_epoch'] = n_e
+			bar.finish()
+			print('\n\nTraining Loss: {}\nValidation Loss: {}\n'
+				  'Best Loss: {}\nBest Epoch: {}\n\n'.format(mean_training_loss,
+				  mean_val_loss, history['best_loss'], history['best_epoch']))
+
+
+def process_mnist_images(batch, output_shape=(128, 128)):
+	images, output_images = batch[0], []
+
+	for img in images:
+		img_gray = np.reshape(img, newshape=(28, 28))
+		rgb_img = np.zeros(shape=(28, 28, 3))
+		rgb_img[:, :, 0] = img_gray
+		rgb_img[:, :, 1] = img_gray
+		rgb_img[:, :, 2] = img_gray
+		rgb_img = skimage.transform.resize(rgb_img, output_shape)
+		output_images.append(rgb_img)
+	return [output_images, batch[1]]
+
+
+def load_training_data(data_dir, data_set) :
+	datasets_root_dir = join(data_dir, 'datasets')
+	if data_set == 'mnist' :
+		return input_data.read_data_sets('MNIST_data', one_hot=True)
+	elif data_set == 'flowers':
+		raise NotImplementedError()
+	elif data_set == 'tobacco':
+		raise NotImplementedError()
+	else :
+		raise NotImplementedError()
+
+def initialize_directories(args):
+	model_dir = join(args.data_dir, 'training', args.model_name)
+	if not os.path.exists(model_dir):
+		os.makedirs(model_dir)
+
+	model_chkpnts_dir = join(model_dir, 'checkpoints')
+	if not os.path.exists(model_chkpnts_dir):
+		os.makedirs(model_chkpnts_dir)
+
+	model_summaries_dir = join(model_dir, 'summaries')
+	if not os.path.exists(model_summaries_dir):
+		os.makedirs(model_summaries_dir)
+
+	model_samples_dir = join(model_dir, 'samples')
+	if not os.path.exists(model_samples_dir):
+		os.makedirs(model_samples_dir)
+
+	model_val_samples_dir = join(model_dir, 'val_samples')
+	if not os.path.exists(model_val_samples_dir):
+		os.makedirs(model_val_samples_dir)
+
+	return model_dir, model_chkpnts_dir, model_samples_dir, \
+		   model_val_samples_dir, model_summaries_dir
+
+
+def save_for_vis(data_dir, real_images, generated_images) :
+	shutil.rmtree(data_dir)
+	os.makedirs(data_dir)
+	for i in range(0, real_images.shape[0]):
+		real_images_255 = (real_images[i, :, :, :])
+		scipy.misc.imsave(join(data_dir, 'real_image_{}.jpg'.format(i)),
+						  real_images_255)
+		fake_images_255 = (generated_images[i, :, :, :])
+		scipy.misc.imsave(join(data_dir, 'fake_image_{}.jpg'.format(i)),
+						  fake_images_255)
+
+if __name__ == '__main__' :
+	main()
